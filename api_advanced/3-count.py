5ed7f16af776c5f51f50cf01760d702d5a9966df
Noelngerem <e.ngerem@alustudent.com>
2023-07-10 04:55:53 +0000

    task 3

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,43 +1,41 @@
 #!/usr/bin/python3
-import requests
 
-def count_words(subreddit, word_list, after=None, count_dict=None):
-    if count_dict is None:
-        count_dict = {}
-    
-    if after is None:
-        url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
-    else:
-        url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100&after={after}"
-    
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
-    
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        
-        for post in posts:
-            title = post['data']['title']
-            lowercase_title = title.lower()
-            
-            for word in word_list:
-                lowercase_word = word.lower()
-                
-                if lowercase_word in lowercase_title:
-                    if lowercase_word in count_dict:
-                        count_dict[lowercase_word] += lowercase_title.count(lowercase_word)
-                    else:
-                        count_dict[lowercase_word] = lowercase_title.count(lowercase_word)
-        
-        after = data['data']['after']
-        
-        if after is not None:
-            return count_words(subreddit, word_list, after, count_dict)
-    
-    sorted_counts = sorted(count_dict.items(), key=lambda x: (-x[1], x[0]))
-    
-    for word, count in sorted_counts:
-        print(f"{word}: {count}")
+import praw
+import re
 
-count_words("python", ["python", "javascript", "java"])
+def count_words(subreddit, word_list):
+  """
+  Queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords.
+
+  Args:
+    subreddit: The name of the subreddit to query.
+    word_list: A list of keywords to count.
+
+  Returns:
+    None.
+  """
+
+  reddit = praw.Reddit(client_id="YOUR_CLIENT_ID",
+                       client_secret="YOUR_CLIENT_SECRET",
+                       user_agent="YOUR_USER_AGENT")
+
+  hot_articles = reddit.subreddit(subreddit).hot(limit=100)
+
+  word_counts = {}
+  for article in hot_articles:
+    title = article.title
+    for word in word_list:
+      word = word.lower()
+      match = re.search(rf"\b{word}\b", title)
+      if match:
+        word_counts[word] = word_counts.get(word, 0) + 1
+
+  sorted_word_counts = sorted(word_counts.items(), key=lambda x: (-x[1], x[0]))
+
+  for word, count in sorted_word_counts:
+    print(f"{word}: {count}")
+
+
+if __name__ == "__main__":
+  count_words("programming", ["python", "javascript", "java"])
+
